d11db3cee4d2d1626ad109656bc43548b865c6d0
haval995 <havalrezan22@gmail.com>
2025-04-21 14:22:47 +0300

    dto added

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,4 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, HttpCode} from '@nestjs/common';
+import { CreateMessageDto} from "./Dtos/createMessage.dto";
 
-@Controller('messages')
-export class MessagesController {}
+@Controller('messages') // class decorator
+export class MessagesController {
+
+    @Get() // method decorator 
+    //body and params are argument decos 
+    listMessages(){
+        return "hey"
+
+    }
+    @Post()
+    createMessage(@Body() body: CreateMessageDto){
+        console.log(body)
+    }
+    @Get("/:id")
+    getMessage(@Param("id") id :string){
+         console.log(id)
+    }
+    
+}
